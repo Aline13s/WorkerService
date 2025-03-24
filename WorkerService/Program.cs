94300353bdb49e053f4fe70cfd9a8aecd7c15b2e
Aline13s <128413126+Aline13s@users.noreply.github.com>
2025-03-24 17:48:13 -0300

    tirar comentarios

--- WorkerService/Program.cs
+++ WorkerService/Program.cs
@@ -8,21 +8,15 @@ using System.IO;
 var builder = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((context, config) =>
     {
-        // Define o diretório base do projeto
         config.SetBasePath(Directory.GetCurrentDirectory());
 
-        // Carrega configurações do appsettings.json
         config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-        // Permite sobrescrever configurações via variáveis de ambiente
         config.AddEnvironmentVariables();
     })
     .ConfigureServices((hostContext, services) =>
     {
-        // Adiciona as configurações de IoC do WorkerService
         services.AddWorkerServices(hostContext.Configuration);
-
-        // Adiciona o Worker como serviço em background
         services.AddHostedService<Worker>();
     })
     .Build();
